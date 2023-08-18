860992e668de20892f009ccaa81095e8306e719e
0xTariq-dev <tariqmohsen222@gmail.com>
2023-08-18 14:05:44 -0700

    Refactor: Adjusted the execution and tokenize functions.

--- exec_cmd.c
+++ exec_cmd.c
@@ -2,24 +2,30 @@
 
 /**
  * exec_cmd - Executes command passed by the shell.
- * @cmd : The command to execute.
+ * @pathname : The path of the command to execute.
+ * @argv: The array of arguments passed to the executable.
  *
  * Return: Always 0.
  */
 int exec_cmd(const char *pathname, char **argv)
 {
+	/** Forking the process pefore exexuting */
 	int id = fork();
 
+	/** Checking for the child process pefore executing */
 	if (id == 0)
 	{
+		/** Return error if NULL was passed */
 		if (pathname == NULL)
 			return (-1);
+		/** Checking for execution status and print error if it fails*/
 		if (execve(pathname, argv, NULL) == -1)
 		{
 			perror("Error");
-			exit(98);
+			exit(-1);
 		}
 	}
+	/** Halting the main process until execution */
 	else
 	{
 		wait(&id);
--- token.c
+++ token.c
@@ -2,17 +2,17 @@
 /**
  * tokenize - tokenize a given string
  * @string: a string to tokinize
- * @delim: a string contain the delimeters
  * note: to free allocated memory
  * 1- free only the first element of argv `argv[0]`
  * 2- free argv
  * Return: a array of strings
  */
-char **tokenize(char *string, char *delim)
+char **tokenize(char *string)
 {
 	char **argv;
 	char *copystr;
 	int toknum, i, ch = 0;
+	char *delim[] = {' ', '\t', '\n'};
 
 	copystr = malloc(strlen(string) + 1);
 	if (!copystr)
@@ -20,9 +20,7 @@ char **tokenize(char *string, char *delim)
 		perror("Error: ");
 		exit(-1);
 	}
-	/* Here is a function that need replace */
 	strcpy(copystr, string);
-	/****************************************/
 	strtok(copystr, delim);
 	for (toknum = 1; strtok(NULL, delim); toknum++)
 	{}
@@ -45,4 +43,3 @@ char **tokenize(char *string, char *delim)
 	argv[i] = NULL;
 	return (argv);
 }
-
