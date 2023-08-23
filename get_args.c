c81749fcad883c063a762f960475b42286c80a50
0xTariq-dev <tariqmohsen222@gmail.com>
2023-08-23 15:38:18 -0700

    Fix: adjust line splitting algorithm

--- get_args.c
+++ get_args.c
@@ -9,33 +9,76 @@
 full_cmd get_args(char *line)
 {
 	full_cmd c;
-	char **argv;
-	int i = 0;
-
 	/** Intializing the structure and allocating it's memory */
-	argv = tokenize(line);
-	c.cmd = argv[0];
-	c.args = argv;
-
-	/** Checking if allocation didn't happen and print Error */
-	if (!c.cmd)
-	{
-		perror("Error: ");
-		exit(-1);
-	}
-	if (!c.args)
-	{
-		perror("Error: ");
-		exit(-1);
-	}
-
-	/** Assigning the arguments to args array */
-	while (argv[i] != NULL)
-	{
-		c.args[i] = argv[i];
-		i++;
-	}
-	c.args[i] = NULL;
-
+	c.args = tokenize(line);
+	c.cmd = c.args[0];
 	return (c);
+}
+
+/**
+ * token_num - get number of tokens in a string
+ * @str: the string
+ * Return: number of tokens in the string or -1 on fail
+ * note: the function doesn't change the original string.
+ */
+int token_num(char *str, char *delim)
+{
+	char *copystr;
+	int num = 0;
+
+	if (str == NULL || delim == NULL)
+		return (-1);
+	copystr = malloc(_strlen(str) + 1);
+	if (!copystr)
+	{
+		perror("Error");
+		exit(-1);
+	}
+	_strcpy(copystr, str);
+	if (strtok(copystr, delim) != NULL)
+		num = 1;
+	while (strtok(NULL, delim))
+		num++;
+	free(copystr);
+	return (num);
+}
+
+/**
+ * tokenize - tokenize a given string
+ * @string: a string to tokinize
+ * note: to free allocated memory
+ * 1- free only the first element of argv `argv[0]`
+ * 2- free argv
+ * Return: a array of strings
+ */
+char **tokenize(char *string)
+{
+	char **argv;
+	char *token;
+	int toknum, i;
+	char delim[] = {' ', '\t', '\n'};
+	/* Get number of tokens to allocate memory for */
+	toknum = token_num(string, delim);
+	/* Allocate memory for tokens and check fail */
+	argv = malloc(sizeof(char *) * (toknum + 1));
+	if (!argv)
+	{
+		perror("Error");
+		exit(-1);
+	}
+	/* Assign the tokens to the array elements */
+	if (toknum == 0)
+	{
+		argv[0] = NULL;
+		return (argv);
+	}
+	else
+	{
+		token = strtok(string, delim);
+		argv[0] = token;
+		for (i = 1; i < toknum; i++)
+			argv[i] = strtok(NULL, delim);
+		argv[i] = NULL;
+		return (argv);
+	}
 }
